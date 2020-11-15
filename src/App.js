8dbe9a37b62f0d75f0731c280ea8c46a8cae71c6
Ene Simona <enesimona30@192-168-0-103.rdsnet.ro>
2020-11-15 10:22:01 +0200

    5. styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
+import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle></GlobalStyle>
       <AboutUs></AboutUs>
     </div>
   );
