689728da5557f4363673c55de3a9da7fc9c03824
Ene Simona <enesimona30@192-168-0-103.rdsnet.ro>
2020-11-15 11:29:23 +0200

    8. Nav

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 import styled from "styled-components";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav></Nav>
       <GlobalStyle></GlobalStyle>
       <AboutUs></AboutUs>
     </div>
