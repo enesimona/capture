4358cbe86bf9843ff1dee9d6669c786c7f648c32
Ene Simona <enesimona30@192-168-0-103.rdsnet.ro>
2020-11-15 11:50:20 +0200

    9. React router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 import styled from "styled-components";
 import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Nav></Nav>
-      <GlobalStyle></GlobalStyle>
-      <AboutUs></AboutUs>
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs></AboutUs>
+        </Route>
+        <Route path="/work">
+          <OurWork></OurWork>
+        </Route>
+        <Route path="/contact">
+          <ContactUs></ContactUs>
+        </Route>
+      </Switch>
     </div>
   );
 }
