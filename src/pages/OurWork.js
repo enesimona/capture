3b51848d427dd2d3fcb3a01eb65389d4a4041925
Ene Simona <enesimona30@192-168-0-103.rdsnet.ro>
2020-11-16 10:24:12 +0200

    20.our work scroll

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -15,8 +15,12 @@ import {
   lineAnim,
   slider,
 } from "../animation";
+import { useScroll } from "../components/useScroll";
 
 const OurWork = () => {
+  const [element, controls] = useScroll();
+  const [element2, controls2] = useScroll();
+
   return (
     <Work
       variants={pageAnimation}
@@ -45,16 +49,23 @@ const OurWork = () => {
           </Hide>
         </NavLink>
       </Movie>
-      <Movie>
+
+      <Movie ref={element} variants={fade} animate={controls} initial="hidden">
         <h2>The Racer</h2>
-        <div className="line"></div>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <NavLink to="/work/the-racer">
           <img src={theracer} alt="the racer"></img>
         </NavLink>
       </Movie>
-      <Movie>
+
+      <Movie
+        ref={element2}
+        variants={fade}
+        animate={controls2}
+        initial="hidden"
+      >
         <h2>Good Times</h2>
-        <div className="line"></div>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <NavLink to="/work/good-times">
           <img src={goodtimes} alt="good times"></img>
         </NavLink>
@@ -72,7 +83,7 @@ const Work = styled(motion.div)`
   }
 `;
 
-const Movie = styled.div`
+const Movie = styled(motion.div)`
   padding-bottom: 10rem;
   .line {
     height: 0.3rem;
